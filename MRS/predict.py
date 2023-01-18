8b607bc8f2e1b529581ba14e09fd73309e69a434
vishwang.dave <vishwang.dave@cgi.com>
2023-01-18 17:51:55 +0100

    modifying directory structure and adding documentation

--- MRS/predict.py
+++ MRS/predict.py
@@ -1,18 +1,24 @@
 import pandas as pd
-import numpy as np
-from .preprocessing import get_preprocessed_data, cleaning
+# import numpy as np
+from preprocessing import get_preprocessed_data, cleaning
 from gensim.models.doc2vec import Doc2Vec
 from nltk.tokenize import word_tokenize
 import logging  # Setting up the loggings to monitor gensim
 
-logging.basicConfig(filename='prediction_logs.log', format="%(levelname)s - %(asctime)s: %(message)s",
+logging.basicConfig(filename='logs/prediction_logs.log', format="%(levelname)s - %(asctime)s: %(message)s",
                     datefmt='%H:%M:%S', level=logging.INFO)
 
 
-
-def predict(lyrics, model_path='C:/TextAI/recommendation system/vishwang/MRS/models/d2v.model'):
+def predict(lyrics, model_path='C:/TextAI/recommendation system/vishwang/MRS/MRS/models/d2v.model'):
+    """
+    function to predict the next few songs based on the lyrics given
+    args:
+        lyrics: multiline text of the lyrics
+    return:
+        recommendations of the songs and other metadata
+    """
     lyrics = cleaning(lyrics)
-    song_details = pd.read_csv('C:/TextAI/recommendation system/vishwang/MRS/preprocessed_data.csv')
+    song_details = pd.read_csv('C:/TextAI/recommendation system/vishwang/MRS/MRS/preprocessed_data.csv')
     model = Doc2Vec.load(model_path)
     # to find the vector of a document which is not in training data
     # sample = str(dfen.sample()['combined'].values)
@@ -20,20 +26,17 @@ def predict(lyrics, model_path='C:/TextAI/recommendation system/vishwang/MRS/mod
     test_data = word_tokenize(lyrics.lower())
     v1 = model.infer_vector(test_data)
     # print("V1_infer", v1)
-    #print(song_details['tags'].head(2))
+    # print(song_details['tags'].head(2))
     sims = model.dv.most_similar([v1])
-    #print(sims)
+    # print(sims)
     most_similar_songs = pd.DataFrame()
     for tag, percentage in sims:
-        #print(type(tag))
-       #print(type(song_details['tags'].iloc[0]))
+        # print(type(tag))
+        # print(type(song_details['tags'].iloc[0]))
         found = pd.DataFrame(song_details[song_details['tags'] == int(tag)])
-        most_similar_songs = pd.concat([most_similar_songs,found])
+        most_similar_songs = pd.concat([most_similar_songs, found])
         t_name = found['track_name']
         # print(found)
         print(
             f"Song: {t_name.values[0]} by {found['track_artist'].values[0]} from album : {found['track_album_name'].values[0]} with similarity percentage: {percentage}")
     return most_similar_songs
-
-
-
--- MRS/preprocessing.py
+++ MRS/preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
+# import argparse
 import random, re
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
@@ -21,6 +21,12 @@ plt.rcParams['figure.figsize'] = (30, 20)
 
 
 def cleaning(text):
+    """
+    function to clean the data for the model
+    args:
+        text: text to clean
+    return: cleaned_data
+    """
     text = re.sub("[^a-zA-Z0-9]", " ", text)
     text = text.lower()
     text = text.split()
@@ -30,7 +36,16 @@ def cleaning(text):
     return text
 
 
-def load_songs(path='C:/TextAI/recommendation system/vishwang/MRS/training_data/spotify_songs.csv'):
+def load_songs(path='C:/TextAI/recommendation system/vishwang/MRS/MRS/training_data/spotify_songs.csv'):
+    """
+    function to preprocess the data and generate the database for the recommendation engine
+    the csv needs to have the same columns as given.
+    args:
+        path : (optional) the path to the csv file
+    returns:
+        dfen: the preprocessed dataframe
+
+    """
     df = pd.read_csv(path)
     dfen = df[df['language'] == 'en']
     dfen.dropna(inplace=True, axis=0)
@@ -43,19 +58,18 @@ def load_songs(path='C:/TextAI/recommendation system/vishwang/MRS/training_data/
 
 
 def get_preprocessed_data():
+    """
+    function to tag the documents and create the final csv for the recommendation engine. this function is the one that
+    needs to be called when starting a new instance of the recommendation engine, or when the songs need to be updated
+
+    saves the final csv in the same folder as 'preprocessed_data.csv'
+    """
     dfen = load_songs()
     dfen['lyrics'] = dfen['lyrics'].apply(cleaning)
-    #print(dfen['lyrics'].head())
     tagged_data = [TaggedDocument(words=word_tokenize(str(_d).lower()), tags=[str(i)]) for i, _d in
                    enumerate(dfen['lyrics'].tolist())]
     song_details = dfen[['track_name', 'track_artist', 'track_album_name', 'track_id','lyrics']]
-    #song_details['tagged_data'] = np.zeros(song_details.shape[0])
     song_details['tags'] = np.zeros(song_details.shape[0])
-    #print(song_details.head())
     for i, t_data in enumerate(tagged_data):
-        # print(i)
-        #print('====================================================')
-        #print(t_data)
-        #song_details['tagged_data'][i] = t_data
         song_details['tags'].iloc[i] = t_data.tags[0]
     song_details.to_csv('preprocessed_data.csv',index=False)
